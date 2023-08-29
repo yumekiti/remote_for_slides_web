3263a26d7ee2b6228a347c2087153597c2e2cae8
yumekiti <yumekiti120440@gmail.com>
2023-08-29 09:54:06 +0900

    feat: github actions and fix initial start

--- public/script/room.js
+++ public/script/room.js
@@ -2,45 +2,49 @@ const note = document.getElementById("note");
 const pageContainer = document.getElementById("pageContainer");
 const page = document.getElementById("page");
 
-const [_, _room, roomId] = location.pathname.split("/");
-const socket = io.connect(location.host, {
-  transports: ['websocket'],
-});
-socket.emit("join", roomId);
+const main = async () => {
+  const [_, _room, roomId] = location.pathname.split("/");
+  const socket = io.connect(location.host, {
+    transports: ['websocket'],
+  });
+  socket.emit("join", roomId);
 
-socket.on("connect", () => {
-  console.log("socket connected");
-});
+  socket.on("connect", () => {
+    console.log("socket connected");
+  });
 
-socket.on("event", (value) => {
-  console.log(value);
-});
+  socket.on("event", (value) => {
+    console.log(value);
+  });
 
-note.style.display = "none";
-pageContainer.style.display = "none";
+  note.style.display = "none";
+  pageContainer.style.display = "none";
 
-socket.on("page", (value) => {
-  pageContainer.style.display = "block";
-  page.textContent = value.page;
-});
+  socket.on("page", (value) => {
+    pageContainer.style.display = "block";
+    page.textContent = value.page;
+  });
 
-socket.on("note", (value) => {
-  note.style.display = "block";
-  note.textContent = value.text;
-});
+  socket.on("note", (value) => {
+    note.style.display = "block";
+    note.textContent = value.text;
+  });
 
-const [prevButton, nextButton] = document.querySelectorAll("button");
-prevButton.addEventListener("click", () => {
-  data = {
-    uuid: roomId,
-    event: "prev",
-  };
-  socket.emit("event", data);
-});
-nextButton.addEventListener("click", () => {
-  data = {
-    uuid: roomId,
-    event: "next",
-  };
-  socket.emit("event", data);
-});
+  const [prevButton, nextButton] = document.querySelectorAll("button");
+  prevButton.addEventListener("click", () => {
+    data = {
+      uuid: roomId,
+      event: "prev",
+    };
+    socket.emit("event", data);
+  });
+  nextButton.addEventListener("click", () => {
+    data = {
+      uuid: roomId,
+      event: "next",
+    };
+    socket.emit("event", data);
+  });
+};
+
+setTimeout(async () => await main(), 300);
